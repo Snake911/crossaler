6a75f5f3246a050766d0d50460d9371ac9dd5c4d
Volkov Danil <snake1234567890-1@yandex.ru>
2023-01-26 13:38:04 +0300

    feat: make last screen and preload

--- script.js
+++ script.js
@@ -1,40 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const Visible = (target) => {
-    // Все позиции элемента
-    const targetPosition = {
-        top: window.pageYOffset + target.getBoundingClientRect().top,
-        left: window.pageXOffset + target.getBoundingClientRect().left,
-        right: window.pageXOffset + target.getBoundingClientRect().right,
-        bottom: window.pageYOffset + target.getBoundingClientRect().bottom
-      },
-      // Получаем позиции окна
-      windowPosition = {
-        top: window.pageYOffset,
-        left: window.pageXOffset,
-        right: window.pageXOffset + document.documentElement.clientWidth,
-        bottom: window.pageYOffset + document.documentElement.clientHeight
-      };
-    return (targetPosition.bottom > windowPosition.top && 
-      targetPosition.top < windowPosition.bottom && 
-      targetPosition.right > windowPosition.left && 
-      targetPosition.left < windowPosition.right);
-  };
-
-  const animateElements = document.querySelectorAll('.animate');
-  const animated = (elements) => {
-    elements.forEach(element => {
-      if(Visible(element)) {
-        element.classList.add('animated');
-      }
-    });
-  };
-
-  animated(animateElements);
-
-  window.addEventListener('scroll', () => {
-    animated(animateElements);
-  });
-
+  
   const dots = document.querySelectorAll('.many-types_dot');
   const examples = tns({
     container: '.many-types_slider',
@@ -139,4 +104,48 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });    
   });
+});
+
+window.addEventListener('load', (event) => {
+  const cover = document.querySelector('.cover');
+  setTimeout(() => {
+    cover.classList.add('hidden');
+  }, 200);
+
+  const Visible = (target) => {
+    // Все позиции элемента
+    const targetPosition = {
+        top: window.pageYOffset + target.getBoundingClientRect().top,
+        left: window.pageXOffset + target.getBoundingClientRect().left,
+        right: window.pageXOffset + target.getBoundingClientRect().right,
+        bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+      },
+      // Получаем позиции окна
+      windowPosition = {
+        top: window.pageYOffset,
+        left: window.pageXOffset,
+        right: window.pageXOffset + document.documentElement.clientWidth,
+        bottom: window.pageYOffset + document.documentElement.clientHeight
+      };
+    return (targetPosition.bottom > windowPosition.top && 
+      targetPosition.top < windowPosition.bottom && 
+      targetPosition.right > windowPosition.left && 
+      targetPosition.left < windowPosition.right);
+  };
+
+  const animateElements = document.querySelectorAll('.animate');
+  const animated = (elements) => {
+    elements.forEach(element => {
+      if(Visible(element)) {
+        element.classList.add('animated');
+      }
+    });
+  };
+
+  animated(animateElements);
+
+  window.addEventListener('scroll', () => {
+    animated(animateElements);
+  });
+  
 });
